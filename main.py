0917952ab39b9d742bc99ab81f8c9def6e715388
Rodriques <rodriques.perry@gmail.com>
2025-12-23 13:31:28 -0600

    complete

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-import requests
-
 # The following API uses hashed psswrd but only the first 5 characters
 # Never use un-hashed passwords
+import requests
+import hashlib
+import sys
 
 
 def request_api_data(query_char):
-    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
-    print(res)
+
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again.')
+
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
     # Check password if it exists in API response
-    pass
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+
+        if count:
+            print(f'{password} was found {count} times... You should probably change your password.')
+        else:
+            print(f'{password} was not found. Carry on!')
+
+    return 'done'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
